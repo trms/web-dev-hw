0c2d1a636e9be16915e290d75ddbe0a28db6e420
Garrett Hughes <battletank130@gmail.com>
2023-06-27 09:43:15 -0500

    Added functionality to deploy to netlify

--- remix.config.js
+++ remix.config.js
@@ -1,5 +1,16 @@
+const baseConfig =
+  process.env.NODE_ENV === "production"
+    ? // when running the Netify CLI or building on Netlify, we want to use
+      {
+        server: "./server.js",
+        serverBuildPath: ".netlify/functions-internal/server.js",
+      }
+    : // otherwise support running remix dev, i.e. no custom server
+      undefined;
+
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
+  ...baseConfig,
   tailwind: true,
   ignoredRouteFiles: ["**/.*"],
   // appDirectory: "app",
