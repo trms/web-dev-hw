008b97f5635682fab14e3f1731c5c7479a62f3e1
Mark <mphill05@gmail.com>
2023-06-28 18:40:50 -0500

    basic funcationality

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,6 +2,7 @@
 import type { V2_MetaFunction } from '@remix-run/node';
 import mainLogo from '../../public/mainLogo.svg';
 import ccLogo from '../../public/cclogo.png';
+import { useState } from 'react';
 
 export const meta: V2_MetaFunction = () => {
   return [
@@ -11,6 +12,62 @@ export const meta: V2_MetaFunction = () => {
 };
 
 export default function Index() {
+  const [submitData, setSubmitData] = useState(false);
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    averageProgramsPerMonth: '',
+    averageLengthOfProgramsInHours: '',
+    needsTranslations: false,
+  });
+  const [apiResponse, setApiResponse] = useState({ lead: null });
+
+  const handleChange = (e: any) => {
+    const { name, value, type, checked } = e.target;
+    const newValue = type === 'checkbox' ? checked : value;
+
+    setFormData((prevData: any) => ({
+      ...prevData,
+      [name]: newValue,
+    }));
+  };
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    setSubmitData(true);
+
+    const form = e.target;
+
+    const data = {
+      averageProgramsPerMonth: Number(
+        form.querySelector('input[name="averageProgramsPerMonth"]').value
+      ),
+      averageLengthOfProgramsInHours: Number(
+        form.querySelector('input[name="averageLengthOfProgramsInHours"]').value
+      ),
+      needsTranslations: form.querySelector('input[name="needsTranslations"]')
+        .checked,
+    };
+
+    try {
+      const response = await fetch('/api/leads', {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      const responseData = await response.json();
+      setApiResponse(responseData);
+    } catch (error) {
+      console.log('Error: ', error);
+    }
+  };
+
+  const yearlyCaptionMins = apiResponse?.lead?.yearlyCaptionMins || 0;
+  console.log(JSON.stringify(yearlyCaptionMins));
+
   return (
     <div className="app w-full h-full min-h-min">
       <div className="bg-[#545c6f] h-24 border-b-[#2EB466] border-b-8">
@@ -87,82 +144,98 @@ export default function Index() {
         </div>
       </div>
       <div className="w-3/4 mx-auto bg-[#EDEEF1] rounded-3xl text-center mb-64">
-        {/* { submit ? ( */}
-        <form className="block pb-12">
-          <div className="flex m-auto justify-left align-left pt-10 pb-6 w-3/4">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              height="1em"
-              viewBox="0 0 448 512"
-              className="fill-[#2EB466] h-16"
+        {!submitData ? (
+          <form className="block pb-12" onSubmit={handleSubmit}>
+            <div className="flex m-auto justify-left align-left pt-10 pb-6 w-3/4">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="1em"
+                viewBox="0 0 448 512"
+                className="fill-[#2EB466] h-16"
+              >
+                <path d="M181.3 32.4c17.4 2.9 29.2 19.4 26.3 36.8L197.8 128h95.1l11.5-69.3c2.9-17.4 19.4-29.2 36.8-26.3s29.2 19.4 26.3 36.8L357.8 128H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H347.1L325.8 320H384c17.7 0 32 14.3 32 32s-14.3 32-32 32H315.1l-11.5 69.3c-2.9 17.4-19.4 29.2-36.8 26.3s-29.2-19.4-26.3-36.8l9.8-58.7H155.1l-11.5 69.3c-2.9 17.4-19.4 29.2-36.8 26.3s-29.2-19.4-26.3-36.8L90.2 384H32c-17.7 0-32-14.3-32-32s14.3-32 32-32h68.9l21.3-128H64c-17.7 0-32-14.3-32-32s14.3-32 32-32h68.9l11.5-69.3c2.9-17.4 19.4-29.2 36.8-26.3zM187.1 192L165.8 320h95.1l21.3-128H187.1z" />
+              </svg>
+              <p className="text-[#545C6D] text-3xl tracking-wider py-4 px-5">
+                Average Number of Programs Per Month:
+              </p>
+              <input
+                className="border-4 border-[#8E97A9] rounded-full text-center"
+                type="number"
+                name="averageProgramsPerMonth"
+                value={formData.averageProgramsPerMonth}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div className="flex m-auto justify-left align-left py-6 w-3/4">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="1em"
+                viewBox="0 0 512 512"
+                className="fill-[#2EB466] h-16"
+              >
+                <path d="M464 256A208 208 0 1 1 48 256a208 208 0 1 1 416 0zM0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM232 120V256c0 8 4 15.5 10.7 20l96 64c11 7.4 25.9 4.4 33.3-6.7s4.4-25.9-6.7-33.3L280 243.2V120c0-13.3-10.7-24-24-24s-24 10.7-24 24z" />
+              </svg>
+              <p className="text-[#545C6D] text-3xl tracking-wider py-4 px-5">
+                Average Length of Program in Hours:
+              </p>
+              <input
+                className="border-4 border-[#8E97A9] rounded-full text-center"
+                type="number"
+                name="averageLengthOfProgramsInHours"
+                value={formData.averageLengthOfProgramsInHours}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div className="flex m-auto justify-left align-left py-6 w-3/4">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="1em"
+                viewBox="0 0 512 512"
+                className="fill-[#2EB466] h-16"
+              >
+                <path d="M352 256c0 22.2-1.2 43.6-3.3 64H163.3c-2.2-20.4-3.3-41.8-3.3-64s1.2-43.6 3.3-64H348.7c2.2 20.4 3.3 41.8 3.3 64zm28.8-64H503.9c5.3 20.5 8.1 41.9 8.1 64s-2.8 43.5-8.1 64H380.8c2.1-20.6 3.2-42 3.2-64s-1.1-43.4-3.2-64zm112.6-32H376.7c-10-63.9-29.8-117.4-55.3-151.6c78.3 20.7 142 77.5 171.9 151.6zm-149.1 0H167.7c6.1-36.4 15.5-68.6 27-94.7c10.5-23.6 22.2-40.7 33.5-51.5C239.4 3.2 248.7 0 256 0s16.6 3.2 27.8 13.8c11.3 10.8 23 27.9 33.5 51.5c11.6 26 20.9 58.2 27 94.7zm-209 0H18.6C48.6 85.9 112.2 29.1 190.6 8.4C165.1 42.6 145.3 96.1 135.3 160zM8.1 192H131.2c-2.1 20.6-3.2 42-3.2 64s1.1 43.4 3.2 64H8.1C2.8 299.5 0 278.1 0 256s2.8-43.5 8.1-64zM194.7 446.6c-11.6-26-20.9-58.2-27-94.6H344.3c-6.1 36.4-15.5 68.6-27 94.6c-10.5 23.6-22.2 40.7-33.5 51.5C272.6 508.8 263.3 512 256 512s-16.6-3.2-27.8-13.8c-11.3-10.8-23-27.9-33.5-51.5zM135.3 352c10 63.9 29.8 117.4 55.3 151.6C112.2 482.9 48.6 426.1 18.6 352H135.3zm358.1 0c-30 74.1-93.6 130.9-171.9 151.6c25.5-34.2 45.2-87.7 55.3-151.6H493.4z" />
+              </svg>
+              <p className="text-[#545C6D] text-3xl tracking-wider py-4 px-5">
+                Do You Need Translations?
+              </p>
+              <label className="relative inline-flex items-center cursor-pointer">
+                <input
+                  type="checkbox"
+                  className="sr-only peer"
+                  name="needsTranslations"
+                  checked={formData.needsTranslations}
+                  onChange={handleChange}
+                />
+                <div className="w-20 h-10 bg-gray-200 peer-focus:outline-none peer-focus:none peer-focus:transparent dark:peer-focus:none rounded-full peer dark:bg-[#8E97A9] peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-4 after:left-[4px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-9 after:w-9 after:transition-all dark:border-[#8E97A9] peer-checked:bg-[#2EB466]"></div>
+              </label>
+            </div>
+            <button
+              className="bg-green-500 hover:bg-green-600 text-white text-3xl tracking-wider py-4 px-5 rounded-full w-64"
+              type="submit"
             >
-              <path d="M181.3 32.4c17.4 2.9 29.2 19.4 26.3 36.8L197.8 128h95.1l11.5-69.3c2.9-17.4 19.4-29.2 36.8-26.3s29.2 19.4 26.3 36.8L357.8 128H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H347.1L325.8 320H384c17.7 0 32 14.3 32 32s-14.3 32-32 32H315.1l-11.5 69.3c-2.9 17.4-19.4 29.2-36.8 26.3s-29.2-19.4-26.3-36.8l9.8-58.7H155.1l-11.5 69.3c-2.9 17.4-19.4 29.2-36.8 26.3s-29.2-19.4-26.3-36.8L90.2 384H32c-17.7 0-32-14.3-32-32s14.3-32 32-32h68.9l21.3-128H64c-17.7 0-32-14.3-32-32s14.3-32 32-32h68.9l11.5-69.3c2.9-17.4 19.4-29.2 36.8-26.3zM187.1 192L165.8 320h95.1l21.3-128H187.1z" />
-            </svg>
-            <p className="text-[#545C6D] text-3xl tracking-wider py-4 px-5">
-              Average Number of Programs Per Month:
-            </p>
-            <input
-              className="border-4 border-[#8E97A9] rounded-full text-center"
-              type="text"
-            />
-          </div>
-          <div className="flex m-auto justify-left align-left py-6 w-3/4">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              height="1em"
-              viewBox="0 0 512 512"
-              className="fill-[#2EB466] h-16"
-            >
-              <path d="M464 256A208 208 0 1 1 48 256a208 208 0 1 1 416 0zM0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM232 120V256c0 8 4 15.5 10.7 20l96 64c11 7.4 25.9 4.4 33.3-6.7s4.4-25.9-6.7-33.3L280 243.2V120c0-13.3-10.7-24-24-24s-24 10.7-24 24z" />
-            </svg>
-            <p className="text-[#545C6D] text-3xl tracking-wider py-4 px-5">
-              Average Length of Program in Hours:
-            </p>
-            <input
-              className="border-4 border-[#8E97A9] rounded-full text-center"
-              type="text"
-            />
-          </div>
-          <div className="flex m-auto justify-left align-left py-6 w-3/4">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              height="1em"
-              viewBox="0 0 512 512"
-              className="fill-[#2EB466] h-16"
-            >
-              <path d="M352 256c0 22.2-1.2 43.6-3.3 64H163.3c-2.2-20.4-3.3-41.8-3.3-64s1.2-43.6 3.3-64H348.7c2.2 20.4 3.3 41.8 3.3 64zm28.8-64H503.9c5.3 20.5 8.1 41.9 8.1 64s-2.8 43.5-8.1 64H380.8c2.1-20.6 3.2-42 3.2-64s-1.1-43.4-3.2-64zm112.6-32H376.7c-10-63.9-29.8-117.4-55.3-151.6c78.3 20.7 142 77.5 171.9 151.6zm-149.1 0H167.7c6.1-36.4 15.5-68.6 27-94.7c10.5-23.6 22.2-40.7 33.5-51.5C239.4 3.2 248.7 0 256 0s16.6 3.2 27.8 13.8c11.3 10.8 23 27.9 33.5 51.5c11.6 26 20.9 58.2 27 94.7zm-209 0H18.6C48.6 85.9 112.2 29.1 190.6 8.4C165.1 42.6 145.3 96.1 135.3 160zM8.1 192H131.2c-2.1 20.6-3.2 42-3.2 64s1.1 43.4 3.2 64H8.1C2.8 299.5 0 278.1 0 256s2.8-43.5 8.1-64zM194.7 446.6c-11.6-26-20.9-58.2-27-94.6H344.3c-6.1 36.4-15.5 68.6-27 94.6c-10.5 23.6-22.2 40.7-33.5 51.5C272.6 508.8 263.3 512 256 512s-16.6-3.2-27.8-13.8c-11.3-10.8-23-27.9-33.5-51.5zM135.3 352c10 63.9 29.8 117.4 55.3 151.6C112.2 482.9 48.6 426.1 18.6 352H135.3zm358.1 0c-30 74.1-93.6 130.9-171.9 151.6c25.5-34.2 45.2-87.7 55.3-151.6H493.4z" />
-            </svg>
-            <p className="text-[#545C6D] text-3xl tracking-wider py-4 px-5">
-              Do You Need Translations?
-            </p>
-            <label className="relative inline-flex items-center cursor-pointer">
-              <input type="checkbox" value="" className="sr-only peer" />
-              <div className="w-20 h-10 bg-gray-200 peer-focus:outline-none peer-focus:none peer-focus:transparent dark:peer-focus:none rounded-full peer dark:bg-[#8E97A9] peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-4 after:left-[4px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-9 after:w-9 after:transition-all dark:border-[#8E97A9] peer-checked:bg-[#2EB466]"></div>
-            </label>
-          </div>
-          <button
-            className="bg-green-500 hover:bg-green-600 text-white text-3xl tracking-wider py-4 px-5 rounded-full w-64"
-            type="submit"
-          >
-            CALCULATE
-          </button>
-        </form>
-        {/* ) : (
-        <h3 className="text-[#545C6D] text-3xl tracking-wider py-12 px-5">
-          You will need approximately
-        </h3>
-        <div>
-          <input
-            className="h-16 w-72 border-4 border-[#8E97A9] rounded-full text-center text-[#545C6D] bg-white"
-            type="text"
-            value={`test`}
-            disabled
-          />
-        </div>
-        <h3 className="text-[#545C6D] text-3xl tracking-wider py-12 pt-5 pb-24">
-          Of Closed Captioning for 1 year
-        </h3>
-        )} */}
+              CALCULATE
+            </button>
+          </form>
+        ) : (
+          <>
+            <h3 className="text-[#545C6D] text-3xl tracking-wider py-12 px-5">
+              You will need approximately
+            </h3>
+            <div>
+              <input
+                className="h-16 w-72 border-4 border-[#8E97A9] rounded-full text-center text-[#545C6D] bg-white"
+                type="text"
+                value={`${yearlyCaptionMins} mins`}
+                disabled
+              />
+            </div>
+            <h3 className="text-[#545C6D] text-3xl tracking-wider py-12 pt-5 pb-24">
+              Of Closed Captioning for 1 year
+            </h3>
+          </>
+        )}
       </div>
     </div>
   );
