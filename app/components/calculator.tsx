96c9beca47d9d46b3759db5d1bde0109156552ae
teresasanders77 <teresasanders77@gmail.com>
2023-06-28 19:17:17 -0600

    Finished responsive styling for calculator form

--- app/components/calculator.tsx
+++ app/components/calculator.tsx
@@ -2,14 +2,14 @@ export default function Calculator() {
     const Form = () => {
         return (
             <form action="/api/leads" method="POST" className="text-2xl">
-                <div className="flex justify-start items-center">
+                <div className="flex justify-start items-center flex-wrap sm:flex-nowrap">
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
                         fill="none"
                         viewBox="0 0 24 24"
                         strokeWidth={1.5}
                         stroke="#26A35C"
-                        className="w-10 h-10 mr-4"
+                        className="w-10 h-10 mr-4 hidden sm:block"
                     >
                         <path
                             strokeLinecap="round"
@@ -17,7 +17,7 @@ export default function Calculator() {
                             d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
                         />
                     </svg>
-                    <label className="mr-4 flex items-center" htmlFor="name">
+                    <label className="sm:mr-4 flex items-center sm:flex-nowrap" htmlFor="name">
                         Name:
                     </label>
                     <input
@@ -27,21 +27,21 @@ export default function Calculator() {
                         className="rounded-3xl border-4 border-medium-grey py-px pl-1.5 text-center"
                     />
                 </div>
-                <div className="flex justify-start items-center my-6">
+                <div className="flex justify-start items-center flex-wrap sm:flex-nowrap my-6">
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
                         fill="none"
                         viewBox="0 0 24 24"
                         strokeWidth={1.5}
                         stroke="#26A35C"
-                        className="w-10 h-10 mr-4"
+                        className="w-10 h-10 mr-4 hidden sm:block"
                     >
                         <path
                             strokeLinecap="round"
                             d="M16.5 12a4.5 4.5 0 11-9 0 4.5 4.5 0 019 0zm0 0c0 1.657 1.007 3 2.25 3S21 13.657 21 12a9 9 0 10-2.636 6.364M16.5 12V8.25"
                         />
                     </svg>
-                    <label className="mr-4 flex items-center" htmlFor="email">
+                    <label className="mr-4 flex items-center md:flex-nowrap" htmlFor="email">
                         Email:
                     </label>
                     <input
@@ -50,14 +50,14 @@ export default function Calculator() {
                         className="rounded-3xl border-4 border-medium-grey py-px pl-1.5 text-center"
                     />
                 </div>
-                <div className="flex justify-start items-center my-6">
+                <div className="flex justify-start items-center flex-wrap sm:flex-nowrap my-6">
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
                         fill="none"
                         viewBox="0 0 24 24"
                         strokeWidth={1.5}
                         stroke="#26A35C"
-                        className="w-10 h-10 mr-4"
+                        className="w-10 h-10 mr-4 hidden sm:block"
                     >
                         <path
                             strokeLinecap="round"
@@ -65,7 +65,7 @@ export default function Calculator() {
                             d="M5.25 8.25h15m-16.5 7.5h15m-1.8-13.5l-3.9 19.5m-2.1-19.5l-3.9 19.5"
                         />
                     </svg>
-                    <label className="mr-4 flex items-center" htmlFor="averageProgramsPerMonth">
+                    <label className="mr-4 flex items-center md:flex-nowrap" htmlFor="averageProgramsPerMonth">
                         Average Number of Programs Per Month:
                     </label>
                     <input
@@ -74,14 +74,14 @@ export default function Calculator() {
                         className="rounded-3xl border-4 border-medium-grey py-px pl-1.5 text-center"
                     />
                 </div>
-                <div className="flex justify-start items-center my-6">
+                <div className="flex justify-start items-center flex-wrap sm:flex-nowrap my-6">
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
                         fill="none"
                         viewBox="0 0 24 24"
                         strokeWidth={1.5}
                         stroke="#26A35C"
-                        className="w-10 h-10 mr-4"
+                        className="w-10 h-10 mr-4 hidden sm:block"
                     >
                         <path
                             strokeLinecap="round"
@@ -90,7 +90,7 @@ export default function Calculator() {
                         />
                     </svg>
 
-                    <label className="mr-4 flex items-center" htmlFor="averageLengthOfProgramsInHours">
+                    <label className="mr-4 flex items-center md:flex-nowrap" htmlFor="averageLengthOfProgramsInHours">
                         Average Length Of Programs In Hours:
                     </label>
                     <input
@@ -99,14 +99,14 @@ export default function Calculator() {
                         className="rounded-3xl border-4 border-medium-grey py-px pl-1.5 text-center"
                     />
                 </div>
-                <div className="flex justify-start items-center  my-6">
+                <div className="flex justify-start items-center flex-wrap sm:flex-nowrap my-6">
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
                         fill="none"
                         viewBox="0 0 24 24"
                         strokeWidth={1.5}
                         stroke="#26A35C"
-                        className="w-10 h-10 mr-4"
+                        className="w-10 h-10 mr-4 hidden sm:block"
                     >
                         <path
                             strokeLinecap="round"
@@ -115,7 +115,7 @@ export default function Calculator() {
                         />
                     </svg>
                     <span className="ml-3 mr-4  text-gray-900 dark:text-gray-300">Do You Need Translations?</span>
-                    <label className="relative inline-flex items-center cursor-pointer">
+                    <label className="relative inline-flex items-center cursor-pointer my-0 mx-auto sm:ml-0">
                         <input type="checkbox" value="" className="sr-only peer" />
                         <div className="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-1 peer-focus:ring-logo-green dark:peer-focus:ring-logo-green dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-logo-green"></div>
                     </label>
@@ -131,7 +131,7 @@ export default function Calculator() {
     };
 
     return (
-        <div className="bg-light-grey container mx-auto rounded-3xl w-7/12 p-10 pl-14 mt-6 mb-10">
+        <div className="bg-light-grey container mx-auto rounded-3xl lg:w-7/12 w-11/12 p-10 lg:pl-14 pl-4 mt-6 lg:mb-10">
             <Form />
         </div>
     );
